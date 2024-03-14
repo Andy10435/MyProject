4cc42c9696ece6265d332ac00f4bb47aff0d9aa8
Andy10435 <115353600+Andy10435@users.noreply.github.com>
2024-03-14 21:26:05 +0800

    Add files via upload

---
+++ MyProject/n09310023/n09310023/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace n09310023
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (btn.Text == "請按我一下")
+                btn.Text = "我已經被按過了";
+            else if (btn.Text == "我已經被按過了")
+                btn.Text = "請按我一下";
+        }
+    }
+}
