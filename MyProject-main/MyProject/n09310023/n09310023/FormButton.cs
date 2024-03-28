dfa7b9c9be8005d12fe3063b3cc6124fd4a5ddc0
Andy10435 <115353600+Andy10435@users.noreply.github.com>
2024-03-28 21:15:04 +0800

    Add files via upload

---
+++ MyProject-main/MyProject/n09310023/n09310023/FormButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace n09310023
+{
+    public partial class FormButton : Form
+    {
+        public FormButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
